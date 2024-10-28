05df3dc27dddbaee25998a4fa924fdeff5b69423
Jailton Júnior DEV <jailtonjr99@hotmail.com.br>
2024-10-27 22:06:42 -0300

    Conexão com o banco de dados e estrutura de projeto definidas

--- bootcamp_new_thinkers/src/main/java/com/jjdev/bootcamp_new_thinkers/BootcampNewThinkersApplication.java
+++ bootcamp_new_thinkers/src/main/java/com/jjdev/bootcamp_new_thinkers/BootcampNewThinkersApplication.java
@@ -1,13 +1,22 @@
 package com.jjdev.bootcamp_new_thinkers;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class BootcampNewThinkersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootcampNewThinkersApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("database_username", Objects.requireNonNull(dotenv.get("DATABASE_USERNAME")));
+        System.setProperty("database_password", Objects.requireNonNull(dotenv.get("DATABASE_PASSWORD")));
+
+        SpringApplication.run(BootcampNewThinkersApplication.class, args);
+    }
 
 }
